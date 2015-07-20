ab83b689ca5faa30cda623e62e08d71d3317319f
zhangxistudy11 <qianfeng@zzxx.local>
2015-07-20 17:05:00 +0800

    dddddd

--- UIButtonDemo2/KGAppDelegate.m
+++ UIButtonDemo2/KGAppDelegate.m
@@ -15,8 +15,43 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(10, 50, 300, 50);
+    //设置按钮显示的标题
+    
+    [button setTitle:@"我是一个按钮,可以点击" forState:UIControlStateNormal];
+    //因为按钮是一个事件驱动型的控件,所以需要去绑定一个事件
+    
+    
+    //一个按钮可以绑定多个事件
+    //多个按钮也可以绑定同一个事件响应的方法
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDown];
+    
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDragInside];
+    
+    
+    UIButton * button2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    button2.frame = CGRectMake(10, 200, 300, 50);
+    //最后一个参数表示此刻按钮的状态，
+    [button2 setTitle:@"OK" forState:UIControlStateNormal];
+    //最后一个参数表示当按钮做什么操作时，执行该方法
+    [button2 addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.window addSubview:button2];
+    
+    
+    
+    [self.window addSubview:button];
+    
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+-(void)buttonClick:(UIButton *)btn
+{
+    NSLog(@"按钮被点击%@",btn.currentTitle);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
